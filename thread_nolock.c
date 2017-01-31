42d9c98e492bfe7d44bb2c84322cd3dc60603af1
Namwoo Kim <iam@kimtree.net>
2017-01-31 23:56:55 +0900

    Modify sample nolock code to include numbered positions

--- thread_nolock.c
+++ thread_nolock.c
@@ -1,20 +1,25 @@
+#include <stdio.h>
 #include <pthread.h>
 pthread_t ntid[8];
 
 struct SharedData
 {
     int count;
+    // (1)
     pthread_mutex_t lock;
+    // (2)
 };
 
 void * thr_fn(void *arg){
     struct SharedData* pd=(struct SharedData*) arg;
     int i;
-
+    // (3)
     for (i=0; i<10000; i++) {
+        // (4)
         pd->count = pd->count + 1;
+        // (5)
     }
-
+    // (6)
     return((void *)0);
 }
 
@@ -23,15 +28,19 @@ int main(void){
     struct SharedData *pd=&d;
     int i;
     d.count=0;
-
+    // (7)
     for (i=0; i<8; i++) {
+        // (8)
         pthread_create(&ntid[i], NULL, thr_fn, (void*)pd);
+        // (9)
     }
     for (i=0; i<8; i++) {
+        // (10)
         pthread_join(ntid[i], NULL);
+        // (11)
     }
-
+    // (12)
     printf("count=%d\n", d.count);
-
+    // (13)
     return 0;
 }
